94ecee6ef344e9ef1b9de14740a34f0253c485c7
Sergio Martin <serjimartsergio@gmail.com>
2023-04-03 12:20:43 +0200

    Add serilog

---
+++ BarberScheduler/BarberScheduler.API/BarberSchedulerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Serilog;
+
+namespace BarberScheduler.API
+{
+    public static class BarberSchedulerExtensions
+    {
+        public static void ConfigureApplicationLogger(this WebApplicationBuilder builder)
+        {
+            var applicationLogger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .CreateLogger();
+
+            builder.Host.UseSerilog(applicationLogger);
+        }
+    }
+}
--- BarberScheduler/BarberScheduler.API/Program.cs
+++ BarberScheduler/BarberScheduler.API/Program.cs
@@ -1,15 +1,76 @@
-var builder = WebApplication.CreateBuilder(args);
+using BarberScheduler.API;
+using Microsoft.OpenApi.Models;
+using Serilog;
 
-var app = builder.Build();
+Console.Title = "BarberScheduler Api";
+var startUpLogger = new LoggerConfiguration()
+    .WriteTo.File("ApplicationStartup.log")
+    .CreateLogger();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+var builder = WebApplication.CreateBuilder(args);
+    builder.ConfigureApplicationLogger();
+
+    builder.Services
+        .AddEndpointsApiExplorer()
+        .AddSwaggerGen(options =>
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "BarberScheduler API",
+            });
+        });
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    ////////////////
+
+
+    var summaries = new[]
+    {
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+    app.MapGet("/weatherforecast", (ILogger<WeatherForecast> logger) =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+
+        logger.LogInformation("Hola mundo");
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+    startUpLogger.Information($"Application startup successful");
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    startUpLogger.Fatal(ex, "Application startup failed");
+    throw;
 }
 
-app.UseHttpsRedirection();
-
-app.Run();
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
